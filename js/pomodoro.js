4c71e4a6833838b057edcdd8c28a056ced70bfe9
nikmargit <nikola.margit@gmail.com>
2017-12-01 00:32:18 +0100

    add functionality to the Time progres bar

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -4,9 +4,12 @@
     var msg = document.getElementById("msg");
     var reset = document.getElementById("reset");
     var timer = document.getElementById("timer");
+    var progresBar = document.getElementById("progres");
     var timeFlag = true;
     var pauseFlag = false;
     var time = 15;
+    var percentage = 1 / time * 100;
+    var percentageCounter = time;
     var interval;
     setTimer(time);
 
@@ -28,15 +31,17 @@
             time = 5;
             timeFlag = !timeFlag;
             msg.textContent = "Chill!";
+            resetBar();
         } else if (time < 0 && !timeFlag) {
             time = 15;
             timeFlag = !timeFlag;
             msg.textContent = "Work Hard!";
+            resetBar();
         }
         setTimer(time);
         time--;
     }
-    function setTimer(time) {
+    function setTimer() {
         var seconds = parseInt(time % 60);
         var minutes = parseInt(time / 60);
         if (minutes < 10) {
@@ -46,11 +51,20 @@
             seconds = "0" + seconds;
         }
         timer.innerHTML = minutes + ":" + seconds;
+        progresBar.style.width = (percentageCounter - time) * percentage + "%";
     }
+
+    function resetBar() {
+        percentage = 1 / time * 100;
+        percentageCounter = time;
+        progresBar.style.width = "0%";
+    }
+
     function resetPomodoro() {
         timeFlag = true;
         pauseFlag = false;
         time = 15;
+        resetBar();
         clearInterval(interval);
         setTimer(time);
         msg.textContent = "Work Hard!";
