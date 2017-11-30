4af66c43b8543ef89197d3ffe80a927243a2fc55
nikmargit <nikola.margit@gmail.com>
2017-11-30 23:33:09 +0100

    add funcionality to Start and Stop buttons

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,35 +1,47 @@
-var start = document.getElementById("start");
-var stop = document.getElementById("stop");
-var msg = document.getElementById("msg");
-var flag = true;
-var time = 1500;
-
-function timer() {
+function setUp() {
+    var start = document.getElementById("start");
+    var stop = document.getElementById("stop");
+    var msg = document.getElementById("msg");
+    var timeFlag = true;
+    var pauseFlag = false;
+    var time = 15;
+    var interval;
     var timer = document.getElementById("timer");
-    var seconds = parseInt(time % 60);
-    var minutes = parseInt(time / 60);
-    if (minutes < 10) {
-        minutes = "0" + minutes;
-    }
-    if (seconds < 10) {
-        seconds = "0" + seconds;
-    }
 
-    timer.innerHTML = minutes + ":" + seconds;
-    time--;
-    if (time < 0 && flag) {
-        time = 300;
-        flag = !flag;
-        msg.textContent = "Chill!";
-    } else if (time < 0 && !flag) {
-        time = 1500;
-        flag = !flag;
-        msg.textContent = "Work Hard!";
+    start.addEventListener("click", function() {
+        if (pauseFlag) return;
+        pauseFlag = true;
+        interval = setInterval(countdown, 1000);
+    });
+
+    stop.addEventListener("click", function() {
+        clearInterval(interval);
+        pauseFlag = false;
+    });
+
+    function countdown() {
+        if (time < 0 && timeFlag) {
+            time = 5;
+            timeFlag = !timeFlag;
+            msg.textContent = "Chill!";
+        } else if (time < 0 && !timeFlag) {
+            time = 15;
+            timeFlag = !timeFlag;
+            msg.textContent = "Work Hard!";
+        }
+
+        var seconds = parseInt(time % 60);
+        var minutes = parseInt(time / 60);
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+
+        timer.innerHTML = minutes + ":" + seconds;
+        time--;
     }
 }
 
-start.addEventListener("click", function(e) {
-    if (e.currentTarget.dataset.triggered) return;
-    e.currentTarget.dataset.triggered = true;
-    setInterval(timer, 1000);
-});
+setUp();
